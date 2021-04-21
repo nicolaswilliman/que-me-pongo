ab8f25fddd969e986cf64c6e3589e549ef02a7c1
willy <williman.nicolas@gmail.com>
2021-04-20 21:04:59 -0300

    added atuendo class and minor correction in prenda

---
+++ src/main/java/vestimentas/Atuendo.java
@@ -0,0 +1,11 @@
+package vestimentas;
+
+import java.util.List;
+
+public class Atuendo {
+  private List<Prenda> prendas;
+
+  public Atuendo(List<Prenda> prendas) {
+    this.prendas = prendas;
+  }
+}
